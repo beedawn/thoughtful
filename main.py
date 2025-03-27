000c9901292a23c1f9cd9a6b5e264421b16cd4ba
Bee Schmersal <brandonschmersal@gmail.com>
2025-03-27 15:32:02 -0400

    completed

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+
+
+
+with open ('data.json', 'r') as file:
+    data = json.load(file)
+    cont = True
+    while (cont is not False):
+        found = False
+        user_question = input("Enter your question (Enter 'q' to quit):\n")
+
+        if user_question.lower() == "q":
+            cont = False
+            break
+        for question in data['questions']:
+            if question['question'] == user_question:
+                print(question['answer'])
+                found = True
+
+        if not found:
+            print("Sorry, I didn't get that, please try again.\n")
+
